33b68f5a8a2273aad902927133fea18af89311a3
merkabamushrooms <118400096+merkabamushrooms@users.noreply.github.com>
2022-11-18 11:04:34 -0600

    Commit from TeleportHQ

--- components/ecwid-shop.js
+++ components/ecwid-shop.js
@@ -7,10 +7,10 @@ const EcwidShop = (props) => {
   return (
     <>
       <div className={`ecwid-shop-container ${props.rootClassName} `}>
-        <div className="ecwid-shop-container1">
-          <div>
-            <DangerousHTML
-              html={`<div id="my-store-78742521"></div>
+        <div className="ecwid-shop-container1"></div>
+        <div className="ecwid-shop-div">
+          <DangerousHTML
+            html={`<div id="my-store-78742521"></div>
 <div>
     <script data-cfasync="false" type="text/javascript"
         src="https://app.ecwid.com/script.js?78742521&data_platform=code&data_date=2022-11-18" charset="utf-8"></script>
@@ -18,8 +18,7 @@ const EcwidShop = (props) => {
         xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-78742521");
     </script>
 </div>`}
-            ></DangerousHTML>
-          </div>
+          ></DangerousHTML>
         </div>
       </div>
       <style jsx>
@@ -36,9 +35,33 @@ const EcwidShop = (props) => {
           .ecwid-shop-container1 {
             flex: 0 0 auto;
             width: auto;
+            border: 2px dashed rgba(120, 120, 120, 0.4);
             height: auto;
             display: flex;
             align-items: flex-start;
+          }
+          .ecwid-shop-div {
+            max-width: 1100px;
+          }
+
+          @media (max-width: 991px) {
+            .ecwid-shop-div {
+              max-width: 991px;
+            }
+          }
+          @media (max-width: 767px) {
+            .ecwid-shop-container1 {
+              padding: var(--dl-space-space-unit);
+            }
+            .ecwid-shop-div {
+              max-width: 776px;
+            }
+          }
+          @media (max-width: 479px) {
+            .ecwid-shop-container {
+              height: 100%;
+              max-width: 478px;
+            }
           }
         `}
       </style>
