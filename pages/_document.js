0f84f10a76db97a26eab71af23834fc97fd75ff1
merkabamushrooms <118400096+merkabamushrooms@users.noreply.github.com>
2022-11-21 12:16:18 -0600

    Commit from TeleportHQ

--- pages/_document.js
+++ pages/_document.js
@@ -43,7 +43,7 @@ class CustomDocument extends Document {
           <noscript
             dangerouslySetInnerHTML={{
               __html:
-                '</noscript><style>\nhtml * {\n    -webkit-font-smoothing: antialiased;\n}\ninput::placeholder, textarea::placeholder {\n   color: #d2d6da;\n}\n</style>\n\n\n<script data-cfasync="false" type="text/javascript"\n    src="https://app.ecwid.com/script.js?78742521&data_platform=code&data_date=2022-11-18" charset="utf-8"></script>\n\n<noscript>',
+                '</noscript><script data-cfasync="false" type="text/javascript"\n    src="https://app.ecwid.com/script.js?78742521&data_platform=code&data_date=2022-11-18" charset="utf-8"></script>\n\n\n<style>\nhtml * {\n    -webkit-font-smoothing: antialiased;\n}\ninput::placeholder, textarea::placeholder {\n   color: #d2d6da;\n}\n</style>\n\n\n<noscript>',
             }}
           ></noscript>
         </Head>
@@ -53,7 +53,7 @@ class CustomDocument extends Document {
           <div
             dangerouslySetInnerHTML={{
               __html:
-                '<script src=\'https://unpkg.com/@teleporthq/teleport-custom-scripts\'></script>\n\n\n<div id="my-store-78742521"></div>\n<div>\n    <script data-cfasync="false" type="text/javascript"\n        src="https://app.ecwid.com/script.js?78742521&data_platform=code&data_date=2022-11-18" charset="utf-8"></script>\n    <script type="text/javascript">\n        xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-78742521");\n    </script>\n</div>',
+                "<script src='https://unpkg.com/@teleporthq/teleport-custom-scripts'></script>\n",
             }}
           ></div>
         </body>
